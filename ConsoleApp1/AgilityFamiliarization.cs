30b57a7b32ef1166e31952dce216afbc2ca34ad4
Kyle Givler <xvegan88x@gmail.com>
2020-11-21 14:25:59 -0500

    Added Krice8989's solution as a project for debugging

---
+++ ConsoleApp1/AgilityFamiliarization.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrawlerFamiliarization
+{
+    public class AgilityFamiliarization
+    {
+        private string _seedLink;
+        private HtmlWeb _web = new HtmlWeb();
+        private HtmlDocument _doc;
+        private List<string> Frontier;
+
+        public AgilityFamiliarization(string seedLink)
+        {
+            _seedLink = seedLink;
+            Frontier = new List<string>();
+        }
+
+        public void Crawl()
+        {
+            BuildFrontier(_seedLink);
+            ExpandFrontier();
+        }
+
+        private void BuildFrontier(string link)
+        {
+            _doc = _web.Load(link);
+            HtmlNode[] foundLinks = _doc.DocumentNode.SelectNodes("//a").ToArray();
+            foreach (HtmlNode node in foundLinks)
+            {
+                string attribute;
+
+                attribute = node.Attributes["href"]?.Value.ToString();
+                if (attribute == null)
+                {
+                    Console.WriteLine("Link was null!");
+                    return;
+                }
+                if (node != null)
+                {
+                    Frontier.Add(node.Attributes["href"].Value.ToString());
+                }
+            }
+        }
+
+        private void ExpandFrontier()
+        {
+            for (int i = 0; i < Frontier.Count; i++)
+            {
+                BuildFrontier(Frontier[i]);
+                Frontier[i].Remove(i);
+            }
+            Console.WriteLine(Frontier.Count);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CrawlerFamiliarization
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            AgilityFamiliarization af = new AgilityFamiliarization("https://google.com");
+            af.Crawl();
+        }
+    }
+}
