ca09cf9691eff435c03b94554f1b2d73325141e3
Kyle Givler <xvegan88x@gmail.com>
2020-11-20 20:38:13 -0500

    Begin working on some basic networking stuff
    Chapter 16 C#7 in a nut shell

---
+++ WebSpiderStuff/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace WebSpiderStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleHelper.DefaultColor = ConsoleColor.Green;
+
+                ConsoleHelper.ColorWriteLine("Please select a demo: ");
+                Console.WriteLine();
+
+
+                ConsoleHelper.ColorWriteLine(ConsoleColor.DarkYellow, "1. Random simple demos");
+                ConsoleHelper.ColorWriteLine(ConsoleColor.DarkYellow, "2. URI Demo");
+                ConsoleHelper.ColorWriteLine(ConsoleColor.DarkYellow, "9. Quit");
+
+                Console.WriteLine();
+                int choice = ConsoleHelper.GetValidInt("Please make your selection: ");
+
+                ExecuteSelection(choice);
+            }
+        }
+
+        public static void ExecuteSelection(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine();
+                    SimpleNetworkingDemos.DemoEntryPoint();
+                    break;
+                case 2:
+                    SimpleNetworkingDemos.UriDemo();
+                    break;
+                case 9:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    ConsoleHelper.ColorWriteLine(ConsoleColor.Red, "\nInvalid selection!\n");
+                    break;
+            }
+        }
+    }
+}
---
+++ WebSpiderStuff/SimpleNetworkingDemos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSpiderStuff
+{
+    public static class SimpleNetworkingDemos
+    {
+        public static void DemoEntryPoint()
+        {
+            // IPEndPoint Demo - IP address port combination
+            Console.WriteLine("IPEndPoint Class represents an ip and port combintation (127.0.0.1:222): ");
+            IPAddress a = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint (a, 222);
+            Console.WriteLine(ep.ToString());
+
+
+
+            Console.WriteLine("\nEnd demo\n");
+        }
+
+        public static void UriDemo()
+        {
+            Console.Write("\nPlease enter a valid URI: ");
+            string uri = Console.ReadLine();
+
+            Uri u = null;
+
+            try
+            {
+                u = new Uri(uri);
+            } 
+            catch(UriFormatException e)
+            {
+                Console.WriteLine("\nWhoa there! That wasn't a valid URI\n");
+            }
+
+            Console.WriteLine($"Scheme: {u.Scheme}");
+            Console.WriteLine($"Host: {u.Host}");
+            Console.WriteLine($"Port: {u.Port}");
+            Console.WriteLine();
+        }
+    }
+}
