4d8e22ff0ac01d9e3c20289ab8d5166959226015
Kyle Givler <xvegan88x@gmail.com>
2021-01-17 12:41:49 -0500

    Console tests

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<ConsoleColor> colors  = new List<ConsoleColor> { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Blue };
+            ConsoleHelper.MulticolorWriteLine(colors, "This is a test!");
+            ConsoleHelper.MulticolorWriteLine(colors, "This is also a test!", true);
+        }
+    }
+}
