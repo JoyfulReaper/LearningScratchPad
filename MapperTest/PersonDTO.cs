aea8d646fac7e5a737ddf678755411a8030a26bb
Kyle Givler <xvegan88x@gmail.com>
2021-01-18 10:34:41 -0500

    Demo of markiel's Mapper

---
+++ MapperTest/PersonDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapperTest
+{
+    public class PersonDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+    }
+}
---
+++ MapperTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MapperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mapper = Mapper.Instance;
+            mapper.Register<Person, PersonDTO>(x => new PersonDTO()
+            {
+                FirstName = x.FirstName,
+                LastName = x.LastName
+            });
+
+            Person test = GetDemoPerson();
+
+            PersonDTO testDTO = mapper.Map<Person, PersonDTO>(test);
+
+            Console.WriteLine($"{testDTO}");
+        }
+
+        private static Person GetDemoPerson() =>
+            new Person()
+            {
+                FirstName = "Test",
+                LastName = "Guy",
+                SSN = "111-11-2548"
+            };
+    }
+}
