b17eb3216befc234bb03459274f88394f51c6ff2
Kyle Givler <xvegan88x@gmail.com>
2021-02-24 19:21:35 -0500

    Just messing around

---
+++ WINFORMSTESTA/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WINFORMSTESTA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            var t = e.Graphics;
+            t.FillEllipse(Brushes.Red, new Rectangle(5, 5, 10, 10));
+
+            t.FillCircle(Brushes.Yellow, 48, 48, 45);
+            t.FillCircle(Brushes.Black, 30, 35, 10);
+            t.FillCircle(Brushes.Black, 65, 35, 10);
+            t.FillCircle(Brushes.Black, 47, 55, 8);
+        }
+    }
+}
---
+++ WINFORMSTESTA/GraphicsExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Wanted something that mimic the OLD VB way of doing it
+// Thank you SO: https://stackoverflow.com/questions/1835062/drawing-circles-with-system-drawing
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace WINFORMSTESTA
+{
+    public static class GraphicsExtensions
+    {
+        public static void DrawCircle(this Graphics g, Pen pen,
+                                      float centerX, float centerY, float radius)
+        {
+            g.DrawEllipse(pen, centerX - radius, centerY - radius,
+                          radius + radius, radius + radius);
+        }
+
+        public static void FillCircle(this Graphics g, Brush brush,
+                                      float centerX, float centerY, float radius)
+        {
+            g.FillEllipse(brush, centerX - radius, centerY - radius,
+                          radius + radius, radius + radius);
+        }
+    }
+}
